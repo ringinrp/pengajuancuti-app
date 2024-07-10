85660033d226664a32c18ae9751e617e705a515f
ringinrp <ringinrp21@gmail.com>
2024-07-10 07:56:45 +0700

    model, migration atasanuser,history,cuti

---
+++ database/migrations/2024_07_09_111225_create_histories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('referensi_id');
+            $table->string('referensi_type');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('user_approve')->nullable();
+            $table->boolean('status_approve')->nullable();
+            $table->boolean('status_approve')->nullable();
+            $table->text('keterangan')->nullable();
+            $table->text('keterangan')->nullable();
+            $table->foreignId('next_approve_id')->nullable()->constrained('users');
+            $table->string('next_approve_id')->nullable();
+            $table->dateTime('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('history');
+    }
+};
